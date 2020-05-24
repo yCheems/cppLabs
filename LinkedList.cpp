89962de9248b600def0e4d6d66440368b1c479cf
yCheems <64150828+yCheems@users.noreply.github.com>
2020-05-24 16:23:04 +0500

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,1 +1,80 @@
-#include "LinkedList.h"
+#include "LinkedList.h"		
+	LinkedList::LinkedList()
+	{
+		Size = 0;
+		head = nullptr;
+	}
+	void LinkedList::Add(int data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node(data);
+		}
+		else
+		{
+			Node* current = this->head;
+			while (current->pNext != nullptr)
+				current = current->pNext;
+			current->pNext = new Node(data);
+		}
+		Size++;
+	}
+	int& LinkedList::operator[](const int index)
+	{
+		int counter = 0;
+		Node* current = this->head;
+		while (current != nullptr)
+		{
+			{
+				if (counter == index)
+					return current->data;
+				current = current->pNext;
+				counter++;
+			}
+		}
+	}
+	void LinkedList::RemoveDuples()
+	{
+		Node* t, * p, * i, * prev;
+		for (p = head; p != nullptr; p = p->pNext)
+		{
+			prev = p;
+			for (i = p->pNext; i != nullptr; )
+			{
+				if (i->data == p->data)
+				{
+					t = prev->pNext = i->pNext;
+					delete(i);
+					i = t;
+					Size--;
+					continue;
+				}
+				prev = i;
+				i = i->pNext;
+			}
+		}
+	}
+	int LinkedList::FindFromEnd(int k)
+	{
+		Node* first = head;
+		Node* second = head;
+		int counter = 1;
+		while (first->pNext != nullptr)
+		{
+			first = first->pNext;
+			counter++;
+			if (counter > k)
+				second = second->pNext;
+		}
+		return second->data;
+	}
+	void LinkedList::Clear()
+	{
+		while (Size)
+		{
+			Node* temp = head;
+			head = head->pNext;
+			delete temp;
+			Size--;
+		}
+	}
